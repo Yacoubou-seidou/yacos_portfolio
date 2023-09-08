0a17c35fc0aa19415a029c812a0a8170c7f879e0
yacoubou-seidou <yacoseidou@gmail.com>
2023-09-08 13:10:45 +0100

    Add projects data

---
+++ src/data/Projects.js
@@ -0,0 +1,108 @@
+export const projects = [
+  {
+    name: 'Intra Codeloccol && Picsine Codeloccol',
+    category: 'Website',
+    status: 'Ongoing',
+    description: 'The Codeloccol intranet learning platform is an invaluable resource for junior web developers to develop their skills and progress in the program.  Piscine Codeloccol is an online platform that aids in selecting junior web developers for Codeloccol. It provides candidates with modules, progress tracking, and review feedback',
+    images: [
+      {
+        name: 'Dashboard',
+        link: './images/previews/intraDash.png'
+      },
+      {
+        name: 'Modules',
+        link: './images/imgs/intraModule.png'
+      },
+      {
+        name: 'Projects',
+        link: './images/imgs/intraProject.png'
+      },
+      {
+        name: 'Projet Details',
+        link: './images/imgs/projectDetails.png'
+      }
+      ,
+      {
+        name: 'Projet Page',
+        link: './images/imgs/intraProjectdetpa.png'
+      }
+    ],
+    link: 'https://main.d3n2iiglzbx8kh.amplifyapp.com/login',
+    preview: './images/previews/intraDash.png',
+    private: true,
+    repo: '',
+    stack: ['React.js', 'Node.js', 'JavaScript', 'useContext', 'Redux', 'Express.js', 'MaterialUi']
+  },
+  {
+    name: 'Codeloccol Website',
+    category: 'Website',
+    status: 'Done',
+    description: 'The website increases Codeloccol visibility and allows the junior web developer to submit their candidature for the program. ',
+    images: [],
+    link: 'https://www.codeloccol.org/',
+    preview: './images/previews/codeloccol2.png',
+    private: true,
+    repo: '',
+    stack: ['HTML', 'CSS', 'React.js', 'Node.js', 'JavaScript', 'TypeScript', 'Express.js']
+  },
+  {
+    name: 'Web Metric',
+    category: 'Website',
+    status: 'Done',
+    description: 'The Web Metric Mobile Application is an innovative and user-friendly mobile application designed to provide users with a comprehensive list of metrics for different countries around the world. It aims to offer a convenient and efficient way for users to access and explore various statistical data related to countries.',
+    images: [],
+    link: 'https://countrydata-ogxs.onrender.com/',
+    preview: './images/previews/country.png',
+    private: false,
+    repo: 'https://github.com/Yacoubou-seidou/React-redux-capstone',
+    stack: ['HTML', 'CSS', 'React.js', 'JavaScript', 'Redux']
+  },
+  {
+    name: 'Pure Fav',
+    category: 'Packages',
+    status: 'In Progress',
+    description: 'Pure Fav Save and edit links effortlessly with this powerful Chrome extension. Easily capture important webpages, rename them for clarity, and organize your digital resources for future reference.',
+    images: [],
+    link: 'https://countrydata-ogxs.onrender.com/',
+    preview: './images/previews/pureFav.png',
+    private: false,
+    repo: 'https://github.com/Yacoubou-seidou/ChromeEX',
+    stack: ['HTML', 'CSS', 'JavaScript', 'Bootstrap']
+  },
+  {
+    name: 'Kondona',
+    category: 'Web application',
+    status: 'Done',
+    description: 'Kondona is a mobile web application where you can manage your budget, you have a list of transactions associated with a category, so that you can see how much money you spent and on what.',
+    images: [],
+    link: 'https://kondona.onrender.com/',
+    preview: './images/previews/kondo.png',
+    private: false,
+    repo: 'https://github.com/Yacoubou-seidou/Kondona',
+    stack: ['HTML', 'CSS', 'JavaScript', 'Ruby on Rails']
+  },
+  {
+    name: 'Console Hub',
+    category: 'Websites',
+    status: 'Done',
+    description: 'Console Hub is the culmination of a collaborative effort by a team of four passionate web developers. Our capstone project brings the world of console gaming to your fingertips. With Console Hub, users can seamlessly rent gaming consoles, unlocking a world of gaming experiences without the commitment of a purchase. Explore our project to see how we combined our skills in web development to create a user-friendly platform for console enthusiasts',
+    images: [],
+    link: 'https://consolehub.netlify.app/',
+    preview: './images/previews/gameConsole.png',
+    private: false,
+    repo: 'https://github.com/Yacoubou-seidou/gaming_console_reservation_front_end',
+    stack: ['HTML', 'CSS', 'JavaScript', 'Ruby on Rails', 'React.js', 'Redux',]
+  },
+  {
+    name: 'Sahelian Dev Website',
+    category: 'Websites',
+    status: 'Ongoing',
+    description: "Sahelian Dev Website: Empowering Sahelian Startups with Online VisibilityAs a freelance team, we took on the mission of enhancing the online presence and accessibility of Sahelian startups through the creation of the Sahelian Dev Website. This dynamic platform serves as a gateway for users to effortlessly connect with Sahelian Dev's services and engage with them online. The integration of a mailer system streamlines communication, allowing users to easily reach out to Sahelian Dev for inquiries, collaboration opportunities, or support. Behind the scenes, our website boasts a powerful backend that facilitates a feature-rich blog, enabling Sahelian Dev to share valuable insights, updates, and resources with the community.",
+    images: ['./images/previews/sahelianPre.png', './images/imgs/sahelian2.png', './images/imgs/sahelian3.png', './images/imgs/sahelian4.png'],
+    link: '',
+    preview: './images/previews/sahelianPre.png',
+    private: true,
+    repo: '',
+    stack: ['HTML', 'CSS', 'JavaScript', 'React.js', 'Redux', 'Express.js', 'Bootstrap']
+  },
+]
