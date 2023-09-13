73f811a0285e8c28e667dc8283f609c7d8fd1eca
yacoubou-seidou <yacoseidou@gmail.com>
2023-09-13 14:06:52 +0100

    responsive 1025

--- src/components/sections/header/Header.jsx
+++ src/components/sections/header/Header.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import NavBar from './navbar/NavBar'
 import Profile from './profile/Profile'
-
 import './header.css'
 const Header = () => {
+
   const [open, setopen] = useState(false)
+  useEffect(() => {
+    const currentLocation = window.location.pathname;
+    if (currentLocation) {
+      setopen(false)
+    }
+  }, [])
   return (
     <>
       <header id="site_header" className={!open ? "header mobile-menu-hide d-flex justify-content-between" : "header animate"}>
