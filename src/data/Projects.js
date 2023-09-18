9ebd615b2b4673f720eb611fa0a43ef82a4be10c
yacoubou-seidou <yacoseidou@gmail.com>
2023-09-18 15:18:30 +0100

    Update project

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,70 +1,70 @@
-// import intraDash from './images/previews/intraDash.png'
-// import intraModule from './images/imgs/intraModule.png'
-// import intraProject from './images/imgs/projectListNew.png'
-// import intraProjectdetails from './images/imgs/projectDetailIntra.png'
-// import intraProjectPage from './images/imgs/projectPageIntra.png'
-// import codeloccol2 from './images/previews/codewebsite.png'
+import intraDash from './images/previews/intraDash.png'
+import intraModule from './images/imgs/intraModule.png'
+import intraProject from './images/imgs/projectListNew.png'
+import intraProjectdetails from './images/imgs/projectDetailIntra.png'
+import intraProjectPage from './images/imgs/projectPageIntra.png'
+import codeloccol2 from './images/previews/codewebsite.png'
 import country from './images/previews/metrics.png'
 import bookStore from './images/previews/bookstores.png'
-// import pureFav from './images/previews/pureFav.png'
+import pureFav from './images/previews/pureFav.png'
 import kondo from './images/previews/kondo.png'
 import consolehub from './images/previews/consoleNew.png'
-// import sahelBanner from './images/previews/sahelianPre.png'
-// import sahelianAbout from './images/imgs/sahelian2.png'
-// import sahelianService from './images/imgs/sahelian3.png'
-// import sahellianContact from './images/imgs/sahelian4.png'
-// import schoolLib from './images/previews/schoolConsole.png'
+import sahelBanner from './images/previews/sahelianPre.png'
+import sahelianAbout from './images/imgs/sahelian2.png'
+import sahelianService from './images/imgs/sahelian3.png'
+import sahellianContact from './images/imgs/sahelian4.png'
+import schoolLib from './images/previews/schoolConsole.png'
 export const projects = [
-  // {
-  //   id: 1,
-  //   name: 'Codeloccol Intranet',
-  //   category: 'Website',
-  //   status: 'In Progress',
-  //   description: "Empowering Junior Web Developers and Identifying Future Talent The Codeloccol intranet serves a dual purpose: it's an essential learning platform for junior web developers seeking to enhance their skills and advance in the program, while also functioning as a robust tool for identifying and evaluating prospective talent for Codeloccol.The Codeloccol Intranet is more than an online platform; it's a dynamic ecosystem that empowers talent development and facilitates the discovery of tomorrow's coding stars within the Codeloccol community.",
-  //   date: '08 January, 2023',
-  //   images: [
-  //     {
-  //       name: 'Dashboard',
-  //       link: intraDash
-  //     },
-  //     {
-  //       name: 'Modules',
-  //       link: intraModule
-  //     },
-  //     {
-  //       name: 'Projects',
-  //       link: intraProject
-  //     },
-  //     {
-  //       name: 'Projet Details',
-  //       link: intraProjectdetails
-  //     }
-  //     ,
-  //     {
-  //       name: 'Projet Page',
-  //       link: intraProjectPage
-  //     }
-  //   ],
-  //   link: 'https://main.d3n2iiglzbx8kh.amplifyapp.com/login',
-  //   preview: intraDash,
-  //   private: true,
-  //   repo: '',
-  //   stack: ['React.js', 'Node.js', 'JavaScript', 'useContext', 'Redux', 'Express.js', 'MaterialUi', 'MongoDB']
-  // },
-  // {
-  //   id: 2,
-  //   name: 'Codeloccol Website',
-  //   category: 'Website',
-  //   status: 'Done',
-  //   description: 'The website increases Codeloccol visibility and allows the junior web developer to submit their candidature for the program. ',
-  //   date: '25 October, 2022',
-  //   images: [{ name: 'Home Page', link: codeloccol2 }],
-  //   link: 'https://www.codeloccol.org/',
-  //   preview: codeloccol2,
-  //   private: true,
-  //   repo: '',
-  //   stack: ['HTML', 'CSS', 'React.js', 'Node.js', 'JavaScript', 'TypeScript', 'Express.js', 'MongoDB']
-  // },
+  {
+    id: 1,
+    name: 'Codeloccol Intranet',
+    category: 'Website',
+    status: 'In Progress',
+    description: "Empowering Junior Web Developers and Identifying Future Talent The Codeloccol intranet serves a dual purpose: it's an essential learning platform for junior web developers seeking to enhance their skills and advance in the program, while also functioning as a robust tool for identifying and evaluating prospective talent for Codeloccol.The Codeloccol Intranet is more than an online platform; it's a dynamic ecosystem that empowers talent development and facilitates the discovery of tomorrow's coding stars within the Codeloccol community.",
+    date: '08 January, 2023',
+    images: [
+      {
+        name: 'Dashboard',
+        link: intraDash
+      },
+      {
+        name: 'Modules',
+        link: intraModule
+      },
+      {
+        name: 'Projects',
+        link: intraProject
+      },
+      {
+        name: 'Projet Details',
+        link: intraProjectdetails
+      }
+      ,
+      {
+        name: 'Projet Page',
+        link: intraProjectPage
+      }
+    ],
+    link: 'https://main.d3n2iiglzbx8kh.amplifyapp.com/login',
+    preview: intraDash,
+    private: true,
+    repo: '',
+    stack: ['React.js', 'Node.js', 'JavaScript', 'useContext', 'Redux', 'Express.js', 'MaterialUi', 'MongoDB']
+  },
+  {
+    id: 2,
+    name: 'Codeloccol Website',
+    category: 'Website',
+    status: 'Done',
+    description: 'The website increases Codeloccol visibility and allows the junior web developer to submit their candidature for the program. ',
+    date: '25 October, 2022',
+    images: [{ name: 'Home Page', link: codeloccol2 }],
+    link: 'https://www.codeloccol.org/',
+    preview: codeloccol2,
+    private: true,
+    repo: '',
+    stack: ['HTML', 'CSS', 'React.js', 'Node.js', 'JavaScript', 'TypeScript', 'Express.js', 'MongoDB']
+  },
   {
     id: 3,
     name: 'Web Metric',
@@ -79,20 +79,20 @@ export const projects = [
     repo: 'https://github.com/Yacoubou-seidou/React-redux-capstone',
     stack: ['HTML', 'CSS', 'React.js', 'JavaScript', 'Redux']
   },
-  // {
-  //   id: 4,
-  //   name: 'Pure Fav',
-  //   category: 'Packages',
-  //   status: 'In Progress',
-  //   description: 'Pure Fav Save and edit links effortlessly with this powerful Chrome extension. Easily capture important webpages, rename them for clarity, and organize your digital resources for future reference.',
-  //   date: '01 April, 2022',
-  //   images: [{ name: 'Chrome view', link: pureFav }],
-  //   link: '',
-  //   preview: pureFav,
-  //   private: false,
-  //   repo: 'https://github.com/Yacoubou-seidou/ChromeEX',
-  //   stack: ['HTML', 'CSS', 'JavaScript', 'Bootstrap']
-  // },
+  {
+    id: 4,
+    name: 'Pure Fav',
+    category: 'Packages',
+    status: 'In Progress',
+    description: 'Pure Fav Save and edit links effortlessly with this powerful Chrome extension. Easily capture important webpages, rename them for clarity, and organize your digital resources for future reference.',
+    date: '01 April, 2022',
+    images: [{ name: 'Chrome view', link: pureFav }],
+    link: '',
+    preview: pureFav,
+    private: false,
+    repo: 'https://github.com/Yacoubou-seidou/ChromeEX',
+    stack: ['HTML', 'CSS', 'JavaScript', 'Bootstrap']
+  },
   {
     id: 5,
     name: 'Kondona',
@@ -135,37 +135,37 @@ export const projects = [
     repo: 'https://github.com/Yacoubou-seidou/Bookstore.git',
     stack: ['HTML', 'CSS', 'JavaScript', 'React.js']
   },
-  // {
-  //   id: 7,
-  //   name: 'Sahelian Dev Website',
-  //   category: 'Websites',
-  //   status: 'Ongoing',
-  //   description: "Sahelian Dev Website: Empowering Sahelian Startups with Online VisibilityAs a freelance team, we took on the mission of enhancing the online presence and accessibility of Sahelian startups through the creation of the Sahelian Dev Website. This dynamic platform serves as a gateway for users to effortlessly connect with Sahelian Dev's services and engage with them online. The integration of a mailer system streamlines communication, allowing users to easily reach out to Sahelian Dev for inquiries, collaboration opportunities, or support. Behind the scenes, our website boasts a powerful backend that facilitates a feature-rich blog, enabling Sahelian Dev to share valuable insights, updates, and resources with the community.",
-  //   date: '10 April, 2023',
-  //   images: [
-  //     { name: 'Banner', link: sahelBanner },
-  //     { name: 'About', link: sahelianAbout },
-  //     { name: 'Service', link: sahelianService },
-  //     { name: 'Contact', link: sahellianContact }
-  //   ],
-  //   link: '',
-  //   preview: sahelBanner,
-  //   private: true,
-  //   repo: '',
-  //   stack: ['HTML', 'CSS', 'JavaScript', 'React.js', 'Redux', 'Express.js', 'Bootstrap', 'MongoDB']
-  // },
-  // {
-  //   id: 8,
-  //   name: 'School Library',
-  //   category: 'Consoles app',
-  //   status: 'Done',
-  //   description: "School library In this project,we  build an console app to record what books are in the library and who borrows them. The app allow you to add a new students or teachers, add new books and save records of who borrowed a given book.",
-  //   date: '30 June, 2023',
-  //   images: [{ name: 'Console View', link: schoolLib }],
-  //   link: '',
-  //   preview: schoolLib,
-  //   private: false,
-  //   repo: 'https://github.com/Yacoubou-seidou/School_library',
-  //   stack: ['Ruby', 'PostgreSQL']
-  // },
+  {
+    id: 7,
+    name: 'Sahelian Dev Website',
+    category: 'Websites',
+    status: 'Ongoing',
+    description: "Sahelian Dev Website: Empowering Sahelian Startups with Online VisibilityAs a freelance team, we took on the mission of enhancing the online presence and accessibility of Sahelian startups through the creation of the Sahelian Dev Website. This dynamic platform serves as a gateway for users to effortlessly connect with Sahelian Dev's services and engage with them online. The integration of a mailer system streamlines communication, allowing users to easily reach out to Sahelian Dev for inquiries, collaboration opportunities, or support. Behind the scenes, our website boasts a powerful backend that facilitates a feature-rich blog, enabling Sahelian Dev to share valuable insights, updates, and resources with the community.",
+    date: '10 April, 2023',
+    images: [
+      { name: 'Banner', link: sahelBanner },
+      { name: 'About', link: sahelianAbout },
+      { name: 'Service', link: sahelianService },
+      { name: 'Contact', link: sahellianContact }
+    ],
+    link: '',
+    preview: sahelBanner,
+    private: true,
+    repo: '',
+    stack: ['HTML', 'CSS', 'JavaScript', 'React.js', 'Redux', 'Express.js', 'Bootstrap', 'MongoDB']
+  },
+  {
+    id: 8,
+    name: 'School Library',
+    category: 'Consoles app',
+    status: 'Done',
+    description: "School library In this project,we  build an console app to record what books are in the library and who borrows them. The app allow you to add a new students or teachers, add new books and save records of who borrowed a given book.",
+    date: '30 June, 2023',
+    images: [{ name: 'Console View', link: schoolLib }],
+    link: '',
+    preview: schoolLib,
+    private: false,
+    repo: 'https://github.com/Yacoubou-seidou/School_library',
+    stack: ['Ruby', 'PostgreSQL']
+  },
 ]
